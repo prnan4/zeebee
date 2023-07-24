833da208752f5d9af33d36ad8cc1a66380819664
Nandhinee <nperiyakaruppan@zuora.com>
2023-07-24 11:45:54 -0700

    Add url component

--- streamlit_custom_chatbot.py
+++ streamlit_custom_chatbot.py
@@ -7,7 +7,17 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.llms import OpenAI
 from langchain.chains import ConversationalRetrievalChain
 
+from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders import TextLoader
+
+from langchain.document_loaders import TextLoader
+import tempfile
+import os
+
+import requests
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import re
 
 # Clear the Session State
 if 'key_to_remove' in st.session_state:
@@ -33,73 +43,113 @@ Simply type your questions or preferences, and I'll provide you with information
 
 Let's get started! How can I assist you today? 😊
 ''')
-         
-# Upload Context Txt file in sidebar
-uploaded_file = st.sidebar.file_uploader("upload", type="text")
 
-# Vectorising custom knowledge
+option = st.sidebar.selectbox("Choose Option for context:", ("Upload a PDF/txt file", "Enter URL"))     
 
-loader = TextLoader("zuora_context.txt")
-documents = loader.load()
+uploaded_file = None
+url = None
+
+if option == "Enter URL":
+    # Get context from URL
+    url = st.sidebar.text_input('Enter URL:')
+else:
+    # Upload Context Txt file in sidebar
+    uploaded_file = st.sidebar.file_uploader("Upload your context file", type=["txt", "pdf"])
 
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-documents = text_splitter.split_documents(documents)
 
-embeddings = OpenAIEmbeddings(openai_api_key = openai_api_key)
-# vectorstore = Chroma.from_documents(documents, embeddings)
-vectorstore = FAISS.from_documents(documents, embeddings)
+def scrape_text_from_url(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
 
-# Creating conversational retrieval chain 
-qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0, openai_api_key = openai_api_key), vectorstore.as_retriever())
+    # Parse the HTML content
+    soup = BeautifulSoup(response.content, "html.parser")
+    def tag_visible(element):
+        if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+            return False
+        if isinstance(element, Comment):
+            return False
+        return True
 
-def conversational_chat(query):
-    
-    result = qa({"question": query, 
-    "chat_history": st.session_state['history']})
-    st.session_state['history'].append((query, result["answer"]))
-    
-    return result["answer"]
+    # Extract visible text from the HTML
+    visible_texts = soup.findAll(string=True)
+    visible_texts = filter(tag_visible, visible_texts)
+    content_text = u" ".join(t.strip() for t in visible_texts)
 
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["Hello! Ask me anything about product catalogue of Zuora" + " 🤗"]
+    # Remove special characters
+    content_text = re.sub(r"[^\w\s]", "", content_text)
 
-if 'history' not in st.session_state:
+    # Remove additional spaces
+    content_text = re.sub(r"\s+", " ", content_text)
+    return content_text
+
+if uploaded_file or url:
+    if uploaded_file:
+        with tempfile.NamedTemporaryFile(mode="wb", delete=False) as tmp_file:
+            tmp_file.write(uploaded_file.getvalue())
+            tmp_file_path = tmp_file.name
+
+        file_extension =  os.path.splitext(uploaded_file.name)[1].lower()  
+
+        if file_extension == ".pdf":
+            loader = PyPDFLoader(file_path=tmp_file_path)  
+            data = loader.load_and_split(text_splitter)
+        
+        elif file_extension == ".txt":
+            loader = TextLoader(file_path=tmp_file_path, encoding="utf-8")
+            data = loader.load_and_split(text_splitter)
+
+    if url: 
+        with open(f"temp.txt", "w") as file:
+            file.write(scrape_text_from_url(url))
+        loader = TextLoader("temp.txt")
+        data = loader.load()
+        documents = text_splitter.split_documents(data)
+        
+    # Defining OpenAI embeddings
+    embeddings = OpenAIEmbeddings(openai_api_key = openai_api_key)
+
+    # vectorstore = Chroma.from_documents(documents, embeddings)
+    vectorstore = FAISS.from_documents(data, embeddings)
+
+    # Creating conversational retrieval chain 
+    qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0, openai_api_key = openai_api_key), vectorstore.as_retriever())
+
+    def conversational_chat(query):
+        
+        result = qa({"question": query, 
+        "chat_history": st.session_state['history']})
+        st.session_state['history'].append((query, result["answer"]))
+        
+        return result["answer"]
+
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hello! Ask me anything about product catalogue of Zuora" + " 🤗"]
+
+    if 'history' not in st.session_state:
         st.session_state['history'] = []
 
-if 'past' not in st.session_state:
-    st.session_state['past'] = ["Hey ! 👋"]
-    
-#container for the chat history
-response_container = st.container()
-#container for the user's text input
-container = st.container()
-
-with container:
-    with st.form(key='my_form', clear_on_submit=True):
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Hey ! 👋"]
         
-        user_input = st.text_input("Query:", placeholder="Enter your question here", key='input')
-        submit_button = st.form_submit_button(label='Enter')
-        
-    if submit_button and user_input:
-        output = conversational_chat(user_input)
-        
-        st.session_state['past'].append(user_input)
-        st.session_state['generated'].append(output)
+    #container for the chat history
+    response_container = st.container()
+    #container for the user's text input
+    container = st.container()
 
-if st.session_state['generated']:
-    with response_container:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            user_input = st.text_input("Query:", placeholder="Enter your question here", key='input')
+            submit_button = st.form_submit_button(label='Enter')
+            
+        if submit_button and user_input:
+            output = conversational_chat(user_input)
+            
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
 
-
-with st.form('response_form'):
-    context_type = st.selectbox('Rate the response', ['Excellent', 'Good', 'Poor'])
-    feedback = st.text_input('More Comments', '')
-    
-    # Feedback is displayed in app. Need to modify logic to store feedback to databaset
-    feedback_submitted = st.form_submit_button('Submit Feedback')
-
-    if feedback_submitted:
-      st.subheader('Feedback:')
-      st.write(feedback)
+    if st.session_state['generated']:
+        with response_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["generated"][i], key=str(i))
